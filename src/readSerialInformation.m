5469397c9d1544be2cf9f45cce07d6147dad2960
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-20 11:47:34 +0200

    Bareboned version to read serial information and prints it out the the command line. Needs functionality to save the values.

--- src/readSerialInformation.m
+++ src/readSerialInformation.m
@@ -1,16 +1,7 @@
-function readSerialInformation(serialPort)
+function out = readSerialInformation()
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     warning on backtrace
-    if strcmp(serialPort.Status, 'closed')
-        fopen(serialPort);
-        if strcmp(serialPort.Status, 'closed')
-            warning('Could not open serial port.');
-        end
-    elseif strcmp(serialPort.Status, 'open')
-        out = fscanf(serialPort)
-    else
-        warning('Unknown error when reading serial port.');
-    end
+    out = evalin('base', 'fscanf(arduinoSerial)')
 end
 
