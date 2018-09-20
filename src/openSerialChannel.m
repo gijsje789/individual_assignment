a96a58bf2b44e0f6ee8b1dc34fcc5aa6a4b49edb
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-20 11:10:33 +0200

    Added success returns

--- src/openSerialChannel.m
+++ src/openSerialChannel.m
@@ -2,6 +2,7 @@ function success = openSerialChannel()
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
     warning on backtrace
+    success = true;
     evalin('base', 'arduinoSerial = serial(serialPort)');
     status = evalin('base', 'arduinoSerial.Status');
     if strcmp(status, 'closed')
@@ -9,17 +10,21 @@ function success = openSerialChannel()
         status = evalin('base', 'arduinoSerial.Status');
         if strcmp(status, 'closed')
             warning('Could not open arduino serial port.');
+            success = false;
         elseif strcmp(status, 'open')
             'Opened arduino COM port.'
             evalin('base', 's.BytesAvailableFcnMode = ''terminator'' ');
             evalin('base', 's.BytesAvailableFcn = @readSerialInformation');
         else
             warning('Unknown error whilst opening COM port.');
+            success = false;
         end
     elseif strcmp(status, 'open')
         warning('COM port already open, possible undetermined behaviour.');
+        success = false;
     else
         warning('Unknown error checking status of COM port.');
+        success = false;
     end
 end
 
