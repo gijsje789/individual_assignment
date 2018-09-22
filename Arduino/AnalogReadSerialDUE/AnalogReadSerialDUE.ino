67629e88acdd4ee02aa615a6ca0f21114f0e5601
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-22 10:05:51 +0200

    Added ISR to count pulses

--- Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
+++ Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
@@ -1,21 +1,33 @@
 // the setup routine runs once when you press reset:
-void setup() {
+volatile int countedPulses = 0;
+void setup() 
+{
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  // Set the analog read resolution to 12-bit.
   analogReadResolution(12); // 12-bit resolution for the ADC
+
+  //Attach the interrupt to the digital pin in order to count the sensor pulses.
+  attachInterrupt(digitalPinToInterrupt(52), countPulses, RISING);
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
+void loop() 
+{
     String string;
   // read the input on analog pin 0:
   int S1Value = analogRead(A0);
   int S2Value = analogRead(A1);
   
   // Send the sensor values through the serial connection.
-  string = "S1," + String(S1Value) + ",S2," + String(S2Value) + ","; 
+  string = "S1," + String(S1Value) + ",S2," + String(S2Value) + ",S3," + String(countedPulses) + ","; 
   // Last comma is needed to seperate the /r/n from last sensor value.
   
   Serial.println(string);
   delay(1);        // delay in between reads for stability
+}
+
+void countPulses() 
+{
+  countedPulses++;
 }
