3580a76965ebda0bf0bc7ef9e5019896cd8a1209
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-23 17:29:27 +0200

    Live plotting of 2 analog sensors

--- src/readSerialInformation.m
+++ src/readSerialInformation.m
@@ -7,15 +7,24 @@ function out = readSerialInformation()
 % out   The character string that is received via the serial communication.
 persistent row;
 persistent data;
+persistent app;
+
+if isempty(app)
+    app = evalin('base', 'app');
+    app.S1Graph.XLimMode = 'manual';
+    app.S1Graph.YLimMode = 'manual';
+    app.S1Graph.YLim = [0, 4150];
 if isempty(row)
     row = 0;
 end
+
     warning on backtrace
     string = evalin('base', 'fscanf(arduinoSerial)');
     splitted = strsplit(string, ',');
     placeIn = -1;
     row = row + 1;
     data(row,1) = now; % timestamp the first cell.
+    data(row,2) = row; % use the row index for plotting.
     for i=1:size(splitted,2)
         if isempty(str2num(splitted{i}))
             % not a number.
@@ -29,7 +38,7 @@ end
             end 
         elseif ~isempty(str2num(splitted{i}))
             if placeIn ~= -1
-                data(row,placeIn+1) = str2num(splitted{i});
+                data(row,placeIn+2) = str2num(splitted{i});
             else
                 warning('Missing sensor information.');
             end
@@ -37,6 +46,24 @@ end
             warning('Unknown error occurred.');
         end
     end
+    
+    if row <= 100
+        xdata = data(1:row,2); % row for plotting.
+        ydata = data(1:row,3:4); % S1 & S2 value
+    else
+        xdata = data(row-100:row,2); % row for plotting
+        ydata = data(row-100:row,3:4); % S1 & S2 value 
+    end
+
+
+%     This should be faster than using plot but there is no line being
+%     shown. Tried animatedLine but that is really slow.
+%     set(app.S1Graph.Children, 'XData', xdata, 'YData', ydata);
+%     drawnow
+
+    plot(app.S1Graph, xdata, ydata);
+    app.S1Graph.XLim = [min(xdata), max(xdata)+40];
+       
     assignin('base', 'sensorData', data);
 end
 
