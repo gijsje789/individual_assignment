c6777480148184d30f8380ab8935e3d4b2608e49
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-24 13:48:33 +0200

    Digital sensors are now stored in base workspace. GUI (see previous commit) calls correct function to store digital sensor parameters to memory.

--- src/setAnalogueSensor.m
+++ src/setAnalogueSensor.m
@@ -1,5 +1,5 @@
 function success = setAnalogueSensor(sensorNumber, output, aVal, bVal)
-%setSensor1 Saves the information about the sensors to the base workspace.
+%setAnalogueSensor Saves the information about the sensor to the base workspace.
 %
 %   INPUTS:
 %   type        The type of sensor, either 'Analogue' or 'Digital'.
@@ -23,11 +23,11 @@ persistent Sensors
         success = true;
         assignin('base', 'analogueSensors', Sensors);
     elseif output <= 0 || output > 3.3
-        warning('Maximum output of sensor %d is not within 0 ... 3.3 V.', sensorNumber)
+        warning('Maximum output of analogue sensor %d is not within 0 ... 3.3 V.', sensorNumber)
     elseif aVal == 0
-        warning('Rico of sensor %d is 0.', sensorNumber)
+        warning('Rico of analogue sensor %d is 0.', sensorNumber)
     elseif bVal < 0 || bVal > 3.3
-        warning('B value of sensor %d is not within 0 ... 3.3 V.', sensorNumber)
+        warning('B value of analogue sensor %d is not within 0 ... 3.3 V.', sensorNumber)
     else % Values are okay.
         Sensors{sensorNumber}.OutputVoltage = output; % Maximum output voltage
         Sensors{sensorNumber}.a = aVal;  % Rico of 'Rico * x + b'
---
+++ src/setDigitalSensor.m
@@ -0,0 +1,28 @@
+function success = setDigitalSensor(sensorNumber, output)
+%setDigitalSensor Saves the information about the sensors to the base workspace.
+%
+%   INPUTS:
+%   type        The type of sensor, either 'Analogue' or 'Digital'.
+%   output      The maximum output of the sensor in pulses per unit. Set output to -1
+%   to disable the sensor. 
+%
+% RETURNS:
+%   success     True when the sensor information is correct. False when one of
+%   the inputs is wrong.
+warning on backtrace
+persistent Sensors
+    success = false;
+    if output == -1
+        Sensors{sensorNumber}.OutputSignal = -1;
+        success = true;
+        assignin('base', 'digitalSensors', Sensors);
+    elseif output <= 0
+        warning('Maximum output of digital sensor %d is not > 0.', sensorNumber)
+    else % Values are okay.
+        Sensors{sensorNumber}.OutputSignal = output; % Maximum output in pulses per minute.
+        success = true;
+        assignin('base', 'digitalSensors', Sensors);
+    end
+
+
+end
