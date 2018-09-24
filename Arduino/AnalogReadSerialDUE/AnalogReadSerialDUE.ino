87c1a3ab578e2893128c7ba6ae9530c6f9810186
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-24 14:42:47 +0200

    Limit the analog reads to 1KHz. Communication (i.e. baudrate) is yet too slow to achieve 1KHz sampling.

--- Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
+++ Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
@@ -1,5 +1,9 @@
 // the setup routine runs once when you press reset:
 volatile int countedPulses = 0;
+int S1Value;
+int S2Value;
+int S3Value;
+unsigned long timestamp;
 void setup() 
 {
   // initialize serial communication at 9600 bits per second:
@@ -15,17 +19,31 @@ void setup()
 void loop() 
 {
     String string;
-  // read the input on analog pin 0:
-  int S1Value = analogRead(A0);
-  int S2Value = analogRead(A1);
-  int S3Value = analogRead(A2);
-  
-  // Send the sensor values through the serial connection.
-  string = "S1," + String(S1Value) + ",S2," + String(S2Value) + ",S3," + String(S3Value) + ",\r\n"; 
-  // Last comma is needed to seperate the /r/n from last sensor value.
-  
-  Serial.print(string);
-  delay(1);        // delay in between reads for stability
+    int delayTime = 0;
+
+    // Timestamp start of main loop.
+    timestamp = micros();
+    // read the input on analog pin 0:
+    S1Value = analogRead(A0);
+    S2Value = analogRead(A1);
+    S3Value = analogRead(A2);
+    
+    // Send the sensor values through the serial connection.
+    string = "S1," + String(S1Value) + ",S2," + String(S2Value) + ",S3," + String(S3Value) + ",\r\n"; 
+    // Last comma is needed to seperate the /r/n from last sensor value.
+    Serial.print(string);
+
+    // Determine processing time and clip to 1KHz.
+    delayTime = 1000 - (micros() - timestamp);
+    if(delayTime>=0)
+    {
+      delayMicroseconds(delayTime);        // delay in between reads for stability
+    }
+    else
+    {
+      delay(0);                 // Do not delay when system is slower than 1KHz.
+    }
+      
 }
 
 void countPulses() 
