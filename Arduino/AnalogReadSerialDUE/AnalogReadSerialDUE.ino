939039cd6cb45b0857c079196e0ed0f356e46b16
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-09-28 12:51:15 +0200

    Seperate variable to state which digital sensor is enabled

--- Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
+++ Arduino/AnalogReadSerialDUE/AnalogReadSerialDUE.ino
@@ -1,9 +1,17 @@
-// the setup routine runs once when you press reset:
+#define NRSENSORS 10
+
+
 volatile int countedPulses = 0;
 int S1Value;
 int S2Value;
 int S3Value;
-unsigned long timestamp;
+String inputString = "";
+bool stringComplete = false;
+bool initComplete = false;
+bool activatedSensors[10] = {false, false, false, false, false, false, false, false, false, false};
+int sensorInformation[3][10] = {};
+
+// the setup routine runs once when you press reset:
 void setup() 
 {
   // initialize serial communication at 115200 bits per second:
@@ -11,13 +19,41 @@ void setup()
   // Set the analog read resolution to 12-bit.
   analogReadResolution(12); // 12-bit resolution for the ADC
 
+  // reserve 200 bytes for the inputString.
+  inputString.reserve(200);
+  
   //Attach the interrupt to the digital pin in order to count the sensor pulses.
   attachInterrupt(digitalPinToInterrupt(52), countPulses, RISING);
-}
+
+  while(!initComplete)
+  {
+    if(stringComplete)
+    {
+      int sensor = 1;
+      for(int i = 0; inputString.length(); i++)
+      {
+        char tempChar = inputString[i];
+        switch(tempChar)
+        {
+          case '1':
+            activatedSensors[sensor-1] = true;
+            sensor++;
+            break;
+          case '0':
+            activatedSensors[sensor-1] = false;
+            sensor++;
+            break;
+          case ',':
+            break;
+        } // Switch
+      } // for
+    } // if
+  } // while
+} // setup
 
 // the loop routine runs over and over again forever:
 void loop() 
-{
+{ 
     String string;
     // read the input on analog pin 0:
     S1Value = analogRead(A0);
@@ -27,7 +63,7 @@ void loop()
     // Send the sensor values through the serial connection.
     string = String(S1Value) + " " + String(S2Value) + " " + String(S3Value) + "\r\n";
     // Last comma is needed to seperate the /r/n from last sensor value.
-    Serial.print(string);
+    // Serial.print(string);
 
     // Necessary for the live plot to update without crashing.
     delay(10);
@@ -37,3 +73,23 @@ void countPulses()
 {
   countedPulses++;
 }
+
+void serialEvent()
+{
+  while(Serial.available())
+  {
+    // Get new byte.
+    char inChar = (char)Serial.read();
+
+    // If the incoming character is a newline, set a flag.
+    // Otherwise add it to the input string.
+    if (inChar == '\n')
+    {
+      stringComplete = true;
+    }
+    else
+    {
+      inputString += inChar;
+    }
+  } // while
+} // serialEvent()
