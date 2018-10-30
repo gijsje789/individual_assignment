4e02b8760dea4594801c3d28dbde7ae621fcf996
Gijs de Vries <33655872+gijsje789@users.noreply.github.com>
2018-10-30 11:29:03 +0100

    Replaced contains with strfind for compatiblity back to 2016

--- src/setSerialPort.m
+++ src/setSerialPort.m
@@ -12,7 +12,7 @@ function success = setSerialPort(port)
 %   success     Returns true when the COM port is valid and set to the base
 %   workspace. Otherwise, false.
     success = true;
-    if contains(port, 'COM')
+    if ~isemtpy(strfind(port, 'COM'))
         [num, status] = str2num(erase(port,'COM'));
         if status
             assignin('base', 'serialPort', port);
